207f362d742268787dc7bc1a969c0d5561c16003
smw7156 <shade.akhtar@yahoo.in>
2022-11-23 20:07:39 +0530

    setting manual text

--- app/src/main/java/com/shadman/test3/MainActivity.kt
+++ app/src/main/java/com/shadman/test3/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textView.text = "this is text"
     }
 }
